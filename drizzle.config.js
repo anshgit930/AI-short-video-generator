610a28e9198c0dad6c6f37cd0b784a6086764970
anshgit930 <anshuujjn@gmail.com>
2026-04-01 17:08:12 +0530

    Security: Hidden DB URL using environment variables

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,7 +3,7 @@ export default {
   schema: "./configs/schema.js",
   dialect: 'postgresql',
   dbCredentials: {
-    url:'postgresql://anshsharma:npg_H6k5MUBvcFJX@ep-polished-glade-adqre2nu-pooler.c-2.us-east-1.aws.neon.tech/ai-short-video-generator?sslmode=require&channel_binding=require',
+    url:process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL,
   }
 };
 
